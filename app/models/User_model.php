e511d28c959bf356cd58151394ae84b2b5a3c9a8
Nathan <71427570+Aqu1n0x@users.noreply.github.com>
2020-10-09 23:00:46 +0700

    tugas PHPMVC

---
+++ app/models/User_model.php
@@ -0,0 +1,8 @@
+<?php 
+class User_model {
+	private $nama = 'Nathan';
+
+	public function getUser() {
+		return $this->nama;
+	}
+}
---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+<h1>About me</h1>
+<img src="<?=BASEURL;?>/img/profile.jpg" alt = "Nathaniel Wijayanto" width="200" class="rounded-circle sh">
+<p>Halo, nama saya <?= $data['nama'];?>, umur saya <?= $data['umur'];?> saya adalah seorang <?= $data['pekerjaan'];?>. </p>
+</div>
